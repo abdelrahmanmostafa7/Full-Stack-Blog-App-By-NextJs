f0808c5f6377376277bfe9510fc31c1520d94851
boyka <abdelrahmanzaki747@gmail.com>
2023-09-26 17:53:06 +0300

    Navbar & responsive Navbar & darkMode Btn

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import "./AuthLinks.module.css";
+import styles from "./AuthLinks.module.css";
+import { useState } from "react";
+
+const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+  const status = "notAuthenticated";
+  return (
+    <>
+      {status === "notAuthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.links}>Logout</span>
+        </>
+      )}
+
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+
+          {status === "notAuthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.links}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthLinks;
